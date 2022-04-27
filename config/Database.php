2d746838b28349e3093b9cdf6ddfb025c4b1ab73
antlogist <antlogist@gmail.com>
2022-04-27 13:14:22 +0300

    DB connection using PDO added

---
+++ config/Database.php
@@ -0,0 +1,25 @@
+<?php 
+
+  class Database {
+    // Database parameters
+    private $host = 'db';
+    private $dbName = 'online_store';
+    private $userName = 'root';
+    private $password = 'root';
+    private $conn;
+
+    // Database connection
+    public function connect() {
+      $this->conn = null;
+
+      try { 
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbName, $this->userName, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+      }
+
+      return $this->conn;
+    }
+
+  }
