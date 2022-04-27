8912e9b0fccbd3b929e7b906bd91b6d09c0033f4
antlogist <antlogist@gmail.com>
2022-04-27 19:33:02 +0300

    Laravel mix and vue added

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php 
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dist/css/all.css">
+    <script src="dist/js/all.js?ver=<?= microtime(); ?>" defer></script>
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
---
+++ resources/js/App.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/cart">Cart</router-link>
+    </div>
+
+  <router-view/>
+
+</template>
+
+<script>
+
+import Home from './components/Home.vue';
+import Cart from './components/Home.vue';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        Home,
+        Cart
+    }
+}
+</script>
