d206414f055bfcd2889b00716edb9f51ccda061f
antlogist <antlogist@gmail.com>
2022-04-28 19:55:11 +0300

    Mitt library added

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import mitt from 'mitt';
+const emitter = mitt();
 
-createApp(App).use(router).use(VueAxios, axios).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+app.use(VueAxios, axios);
+app.config.globalProperties.emitter = emitter;
+
+app.mount('#app');
