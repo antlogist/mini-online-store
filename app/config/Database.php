fb3bce0ac7f07e81bc77320469b1e79b162633bb
antlogist <antlogist@gmail.com>
2022-04-29 21:45:35 +0300

    utf-8 encoding fixed

--- app/config/Database.php
+++ app/config/Database.php
@@ -13,7 +13,7 @@
       $this->conn = null;
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbName, $this->userName, $this->password);
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbName, $this->userName, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
